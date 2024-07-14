1e096908b945cacbc3f403ebdded6e312437c3d5
Kenneth Cox <damitwhy@gmail.com>
2024-07-14 23:28:19 +0000

    Connected to APIs and confired access to google sheet

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+# Define the scope
+scope = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+]
+
+# Define the credentials
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(scope)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+
+# Define the sheet
+SHEET = GSPREAD_CLIENT.open("love_sandwiches")
+
+# Get the data from the sheet
+sales = SHEET.worksheet("sales")
+
+data = sales.get_all_values()
+
+print(data)
